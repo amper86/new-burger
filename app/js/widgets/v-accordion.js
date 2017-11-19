c3906572106cc15644da8beab6f5609cc27895eb
Oleg Mullakaev <o.mullakaev@gmail.com>
2017-11-19 14:12:08 +0300

    vacc add outerHeight

--- app/js/widgets/v-accordion.js
+++ app/js/widgets/v-accordion.js
@@ -7,13 +7,34 @@ $(function () {
             item = $this.closest('.accordeon-list__item'),
             list = $this.closest('.accordeon-list'),
             items = list.find('.accordeon-list__item'),
-            activeClass = 'accordeon-list__item_active';
+            activeClass = 'accordeon-list__item_active',
+            content = $('.accordeon-list__content', item),
+            otherContent = $('.accordeon-list__content', list),
+            description = $('.accordeon-list__description', item),
+            descriptionHeight = description.outerHeight(),
+            picture = $('.accordeon-list__picture', item),
+            pictureHeight = picture.outerHeight(),
+            reqHeight = descriptionHeight + pictureHeight;
+
 
         if (!item.hasClass(activeClass)) {
             items.removeClass(activeClass);
             item.addClass(activeClass);
+
+            otherContent.css({
+                'height': 0
+            });
+
+            content.css({
+                'height': reqHeight
+            })
+
         } else {
             item.removeClass(activeClass);
+
+            content.css({
+                'height': 0
+            })
         }
     });
 });
