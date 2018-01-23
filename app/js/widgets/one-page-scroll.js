6212378127f1e83a5246916e6228e8c40502b602
Oleg Mullakaev <o.mullakaev@gmail.com>
2018-01-23 16:44:34 +0300

    add functions nav buttons

--- app/js/widgets/one-page-scroll.js
+++ app/js/widgets/one-page-scroll.js
@@ -4,7 +4,7 @@ $(function () {
         sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
         easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
                                          // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-        animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+        animationTime: 300,             // AnimationTime let you define how long each section takes to animate
         pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
         updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
         beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
@@ -21,6 +21,14 @@ $(function () {
     $('.scroll').on('click', function (e) {
         e.preventDefault();
         $('.wrapper').moveDown();
+    });
+
+    $('.nav__link, .order-button').on('click', function (e) {
+        e.preventDefault();
+
+        var pageNumber = $(this).attr('data-nav');
+
+        $('.wrapper').moveTo(pageNumber);
     })
 
 });
